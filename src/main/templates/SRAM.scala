57275ee07f21bb0ab19fa53656773930eee5049a
raghu <raghup17@gmail.com>
2016-07-06 19:54:54 -0700

    Move tests to be in the same file (and package) as the module

--- src/main/templates/SRAM.scala
+++ src/main/templates/SRAM.scala
@@ -20,3 +20,31 @@ class SRAM(val w: Int, val d: Int) extends Module {
   }
   io.rdata := mem(raddr_reg)
 }
+
+class SRAMTests(c: SRAM) extends Tester(c) {
+
+  // Set memory
+  for (i <- 0 until c.d) {
+    val x1 = if (i%2 == 0) BigInt("A", 16) else BigInt("B", 16)
+    pokeAt(c.mem, x1, i)
+  }
+
+  for (n <- 0 until c.d) {
+        poke(c.io.raddr, n)
+        val rdataBefore = peek(c.io.rdata)
+        step(1)
+        val rdataAfter = peek(c.io.rdata)
+  }
+}
+
+object SRAMTest {
+  val w = 4
+  val d = 8
+
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(args, () => Module(new SRAM(w, d))) {
+      c => new SRAMTests(c)
+    }
+  }
+}
+
--- src/test/SRAMTest.scala
+++ src/test/SRAMTest.scala
@@ -3,30 +3,4 @@ package plasticine.test
 import Chisel._
 import plasticine.templates.SRAM
 
-class SRAMTests(c: SRAM) extends Tester(c) {
-
-  // Set memory
-  for (i <- 0 until c.d) {
-    val x1 = if (i%2 == 0) BigInt("A", 16) else BigInt("B", 16)
-    pokeAt(c.mem, x1, i)
-  }
-
-  for (n <- 0 until c.d) {
-        poke(c.io.raddr, n)
-        val rdataBefore = peek(c.io.rdata)
-        step(1)
-        val rdataAfter = peek(c.io.rdata)
-  }
-}
-
-object SRAMTest {
-  val w = 4
-  val d = 8
-
-  def main(args: Array[String]): Unit = {
-    chiselMainTest(args, () => Module(new SRAM(w, d))) {
-      c => new SRAMTests(c)
-    }
-  }
-}
 
