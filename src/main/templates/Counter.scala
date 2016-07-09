51dd2efe41ceaca0fd80546a22c1727047eca362
raghu <raghup17@gmail.com>
2016-07-09 13:13:18 -0700

    Widen the 'count' wire by 1 bit to account for addition overflow. Previously, since both 'count' and 'stride' are declared with the same bitwidth, the addition was also represented using same number of bits, which caused issues with overflow (when say stride == max-1).

--- src/main/templates/Counter.scala
+++ src/main/templates/Counter.scala
@@ -27,9 +27,9 @@ class Counter(val w: Int) extends Module {
   reg.io.data.init := init
   reg.io.control.enable := io.control.enable
 
-  val count = reg.io.data.out
+  val count = Cat(UInt(0, width=1), reg.io.data.out)
   val newval = count + io.data.stride
-  val isMax = newval === io.data.max
+  val isMax = newval >= io.data.max
 
   when (io.control.reset) {
     reg.io.data.in := init
@@ -48,40 +48,51 @@ class CounterTests(c: Counter) extends Tester(c) {
   val saturationVal = (1 << c.w) - 1  // Based on counter width
 
   val max = 10 % saturationVal
-  val stride = 1
+  val stride = 6
   val saturate = 0
+
   poke(c.io.data.max, max)
   poke(c.io.data.stride, stride)
   poke(c.io.control.enable, 1)
   poke(c.io.control.saturate, saturate)
   poke(c.io.control.reset, 0)
 
-  println("[reset = 0, enable = 1]")
-  expect(c.io.data.out, 0)
-  expect(c.io.control.done, 0)
+  val expectedCounts = 0 until max by stride toList
 
-  def testOneStep(i: Int) = {
+  var numEnabledCycles = 0
+  var expectedCount = 0
+  var expectedDone = 0
+  def testOneStep() = {
     step(1)
-    val (expectedCount, expectedDone) = saturate match {
+    numEnabledCycles += 1
+    val (count, done) = saturate match {
       case 1 =>
-        val count = if (i < max) i else max-1
-        val done = if (i < max-1) 0 else 1
+        val count = if (numEnabledCycles < expectedCounts.size) expectedCounts(numEnabledCycles) else expectedCounts.last
+        val done = if (count == expectedCounts.last) 1 else 0
         (count, done)
       case 0 =>
-        val count = i % max
-        val done = if (math.abs(i%max - (max-1)) < stride)  1 else 0
+        val count = expectedCounts(numEnabledCycles % expectedCounts.size)
+        val done = if (count == expectedCounts.last)  1 else 0
         (count, done)
     }
-    expect(c.io.data.out, expectedCount)
-    expect(c.io.control.done, expectedDone)
+    expect(c.io.data.out, count)
+    expect(c.io.control.done, done)
+    expectedCount = count
+    expectedDone = done
   }
+
+  println("[reset = 0, enable = 1]")
+  expect(c.io.data.out, expectedCount)
+  expect(c.io.control.done, expectedDone)
+
   for (i <- 1 until (max+10)) {
-    testOneStep(i)
+    testOneStep()
   }
 
   println("[reset = 1, enable = 1]")
   poke(c.io.control.reset, 1)
-  expect(c.io.data.out, max-1)
+  numEnabledCycles = 0
+  expect(c.io.data.out, expectedCounts.last)
   expect(c.io.control.done, 1)
   for (i <- 0 until 5) {
     step(1)
@@ -103,24 +114,22 @@ class CounterTests(c: Counter) extends Tester(c) {
   expect(c.io.data.out, 0)
   expect(c.io.control.done, 0)
   for (i <- 1 until 5) {
-    testOneStep(i)
+    testOneStep()
   }
-  val currentCount = 4
-
   println("[reset = 0, enable = 0]")
   poke(c.io.control.enable, 0)
   for (i <- 0 until 5) {
     step(1)
-    expect(c.io.data.out, currentCount)
-    expect(c.io.control.done, 0)
+    expect(c.io.data.out, expectedCount)
+    expect(c.io.control.done, expectedDone)
   }
 
   println("[reset = 0, enable = 1]")
   poke(c.io.control.enable, 1)
-  expect(c.io.data.out, currentCount)
-  expect(c.io.control.done, 0)
-  for (i <- currentCount+1 until max+10) {
-    testOneStep(i)
+  expect(c.io.data.out, expectedCount)
+  expect(c.io.control.done, expectedDone)
+  for (i <- 1 until max+10) {
+    testOneStep()
   }
 }
 
