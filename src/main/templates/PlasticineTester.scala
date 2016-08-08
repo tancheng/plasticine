f88d80714c829aa2b2394f3886fb7b268afa6926
raghu <raghup17@gmail.com>
2016-08-08 09:42:59 -0700

    Add sim helper functions to read and write BRAMs.

--- src/main/templates/PlasticineTester.scala
+++ src/main/templates/PlasticineTester.scala
@@ -13,4 +13,15 @@ class PlasticineTester[+T <: Module](c: T, isTrace: Boolean = true, _base: Int =
     dumpFile: Option[String] = None) extends Tester(c, isTrace, _base, testCmd, dumpFile) {
   type Inst = HashMap[String, Int]
 
+  def setMem[A <: Bits](dst: Mem[A], src: Array[Int]) {
+    for (i <- 0 until src.size) {
+      pokeAt(dst, BigInt(src(i)), i)
+    }
+  }
+
+  def getMem[A <: Bits](src: Mem[A], size: Int): Array[Int] = {
+    Array.tabulate(size) { i =>
+      peekAt(src, i).toInt
+    }
+  }
 }
