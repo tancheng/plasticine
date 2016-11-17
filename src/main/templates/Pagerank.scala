a08b125494fb8445c5fc39b7ba258ab026c5e77a
raghu <raghup17@gmail.com>
2016-11-17 15:09:04 -0800

    Add Pagerank and (what will eventually become) parallel

--- src/main/templates/Pagerank.scala
+++ src/main/templates/Pagerank.scala
@@ -14,79 +14,93 @@ class Pagerank(val numInputs: Int) extends Module {
   }
 
   //  Sequential top {
-  //    Metapipe m1 {
-  //      TileLoad() Ctr
-  //      TileLoad() Ctr
-  //      Metapipe m2 {
-  //        Parallel p1()
-  //        Pipe p2()
-  //        Tilest() Ctr
-  //        Pipe p3
-  //        Pipe p4
+  //    Sequential m1 {
+  //      TileLoad() tl1 Ctr
+  //      TileLoad() tl2 Ctr
+  //      TileLoad() tl3 Ctr
+  //      Sequential m2 {
+  //        Pipe
+  //        TileLoad() tl4 ctr
+  //        TileLoad() tl5 ctr
+  //        Pipe
+  //        Pipe
+  //        Gather
+  //        Pipe
+  //        Pipe
   //      }
+  //      tileSt
   //    }
-  //    TileSt
   //  }
-
-//  val top = Module(new Sequential(2))
-//  top.io.numIter := UInt(10)
-//  top.io.enable := io.enable
-//  io.done := top.io.done
-
-//  val m1 = Module(new Metapipe(3))
-//  m1.io.enable := top.io.stageEnable(0)
-//  top.io.stageDone(0) := m1.io.done
-
-//  val m2 = Module(new Metapipe(5))
-//  m2.io.enable := top.io.stageEnable(1)
-//  top.io.stageDone(1) := m2.io.done
 
   val m1 = Module(new Metapipe(3))
   m1.io.numIter := UInt(10)
   m1.io.enable := io.enable
   io.done := m1.io.done
 
-  val p2 = Module(new Counter(32))
-  p2.io.control.saturate := Bool(false)
-  p2.io.control.reset := Bool(false)
-  p2.io.data.max := UInt(5)
-  p2.io.data.stride := UInt(1)
-  p2.io.control.enable := m1.io.stageEnable(0)
-  m1.io.stageDone(0) := p2.io.control.done
+  /** To be replaced with actual tile load stuff that talks to the MU */
+  val t1 = Module(new Counter(32))
+  t1.io.control.saturate := Bool(false)
+  t1.io.control.reset := Bool(false)
+  t1.io.data.max := UInt(5)
+  t1.io.data.stride := UInt(1)
+  t1.io.control.enable := m1.io.stageEnable(0)
+  m1.io.stageDone(0) := t1.io.control.done
 
-  val p3 = Module(new Counter(32))
-  p3.io.control.saturate := Bool(false)
-  p3.io.control.reset := Bool(false)
-  p3.io.data.max := UInt(8)
-  p3.io.data.stride := UInt(1)
-  p3.io.control.enable := m1.io.stageEnable(1)
-  m1.io.stageDone(1) := p3.io.control.done
+  val t2 = Module(new Counter(32))
+  t2.io.control.saturate := Bool(false)
+  t2.io.control.reset := Bool(false)
+  t2.io.data.max := UInt(5)
+  t2.io.data.stride := UInt(1)
+  t2.io.control.enable := m1.io.stageEnable(1)
+  m1.io.stageDone(1) := t2.io.control.done
 
-  val p4 = Module(new Counter(32))
-  p4.io.control.saturate := Bool(false)
-  p4.io.control.reset := Bool(false)
-  p4.io.data.max := UInt(5)
-  p4.io.data.stride := UInt(3)
-  p4.io.control.enable := m1.io.stageEnable(2)
-  m1.io.stageDone(2) := p4.io.control.done
+  val m2 = Module(new Metapipe(5))
+  m2.io.numIter := UInt(10)
+  m2.io.enable := m1.io.stageEnable(2)
+  m1.io.stageDone(2) := m2.io.done
+
+    val p1 = Module(new Counter(32))
+    p1.io.control.saturate := Bool(false)
+    p1.io.control.reset := Bool(false)
+    p1.io.data.max := UInt(5)
+    p1.io.data.stride := UInt(1)
+    p1.io.control.enable := m2.io.stageEnable(0)
+    m2.io.stageDone(0) := p1.io.control.done
+
+    val p2 = Module(new Counter(32))
+    p2.io.control.saturate := Bool(false)
+    p2.io.control.reset := Bool(false)
+    p2.io.data.max := UInt(5)
+    p2.io.data.stride := UInt(1)
+    p2.io.control.enable := m2.io.stageEnable(1)
+    m2.io.stageDone(1) := p2.io.control.done
+
+    val t3 = Module(new Counter(32))
+    t3.io.control.saturate := Bool(false)
+    t3.io.control.reset := Bool(false)
+    t3.io.data.max := UInt(5)
+    t3.io.data.stride := UInt(1)
+    t3.io.control.enable := m2.io.stageEnable(2)
+    m2.io.stageDone(2) := t3.io.control.done
+
+
+    val p3 = Module(new Counter(32))
+    p3.io.control.saturate := Bool(false)
+    p3.io.control.reset := Bool(false)
+    p3.io.data.max := UInt(8)
+    p3.io.data.stride := UInt(1)
+    p3.io.control.enable := m2.io.stageEnable(3)
+    m2.io.stageDone(3) := p3.io.control.done
+
+    val p4 = Module(new Counter(32))
+    p4.io.control.saturate := Bool(false)
+    p4.io.control.reset := Bool(false)
+    p4.io.data.max := UInt(5)
+    p4.io.data.stride := UInt(3)
+    p4.io.control.enable := m2.io.stageEnable(4)
+    m2.io.stageDone(4) := p4.io.control.done
 }
-
-
-class PagerankTests(c: Pagerank) extends PlasticineTester(c) {
-  val maxCycles = 100
-  var done = 0
-  var elapsed = 0
-
-  poke(c.io.enable, 1)
-  while ((done != 1) & elapsed < maxCycles) {
-    step(1)
-    elapsed += 1
-    done = peek(c.io.done).toInt
-  }
-
-}
-
-
+class PagerankTests(c: Pagerank) extends Tester(c) {}
 object PagerankTest {
 
   def main(args: Array[String]): Unit = {
---
+++ src/main/templates/Parallel.scala
@@ -0,0 +1,181 @@
+package plasticine.templates
+
+import Chisel._
+
+import plasticine.pisa.parser.Parser
+import plasticine.pisa.ir._
+
+import scala.collection.mutable.HashMap
+
+class Parallel(val numInputs: Int) extends Module {
+  val io = new Bundle {
+    val enable = Bool(INPUT)
+    val done = Bool(OUTPUT)
+    val numIter = UInt(INPUT, width=32)
+    val stageEnable = Vec.fill(numInputs) { Bool(OUTPUT) }
+    val stageDone = Vec.fill(numInputs) { Bool(INPUT) }
+  }
+
+  // 0: INIT, 1: RESET, 2..2+numInputs-1: stages, numInputs: DONE
+  val initState = 0
+  val resetState = 1
+  val fillState = resetState + 1
+  val steadyState = fillState + numInputs - 1
+  val drainState = steadyState + 1
+  val doneState = drainState+numInputs-1
+
+  val stateFF = Module(new FF(32))
+  stateFF.io.control.enable := Bool(true)
+  stateFF.io.data.init := UInt(0)
+  val state = stateFF.io.data.out
+
+  // Counter for num iterations
+  val maxFF = Module(new FF(32))
+  maxFF.io.control.enable := io.enable
+  maxFF.io.data.in := io.numIter
+  val max = maxFF.io.data.out
+
+  val ctr = Module(new Counter(32))
+  ctr.io.control.enable := io.enable & io.stageDone(0)
+  ctr.io.control.reset := (state === UInt(doneState))
+  ctr.io.control.saturate := Bool(false)
+  ctr.io.data.max := max
+  ctr.io.data.stride := UInt(1)
+  val iter = ctr.io.data.out
+
+  val doneClear = UInt(width=1)
+  val doneFF = List.tabulate(numInputs) { i =>
+    val ff = Module(new FF(1))
+    ff.io.control.enable := io.stageDone(i) | doneClear
+    ff.io.data.init := UInt(0)
+    ff.io.data.in := Mux(io.stageDone(i), io.stageDone(i), UInt(0))
+    ff
+  }
+
+  val doneMask = doneFF.map { _.io.data.out }
+
+  // Provide default value for enable and doneClear
+  io.stageEnable.foreach { _ := UInt(0) }
+  doneClear := UInt(0)
+
+  when(io.enable) {
+    when(state === UInt(initState)) {   // INIT -> RESET
+      stateFF.io.data.in := UInt(resetState)
+    }.elsewhen (state === UInt(resetState)) {  // RESET -> FILL
+      stateFF.io.data.in := UInt(fillState)
+    }.elsewhen (state < UInt(steadyState)) {  // FILL -> STEADY
+      for ( i <- fillState until steadyState) {
+        val fillStateID = i - fillState
+        when((state === UInt(i))) {
+          io.stageEnable.zip(doneMask).take(fillStateID+1).foreach { case (en, done) => en := ~done }
+          io.stageEnable.drop(fillStateID+1).foreach { en => en := UInt(0) }
+          val doneTree = Bool(doneMask.take(fillStateID+1).reduce {_&_})
+          doneClear := doneTree
+
+          when (doneTree) {
+            stateFF.io.data.in := UInt(i+1)
+          }.otherwise {
+            stateFF.io.data.in := state
+          }
+        }
+      }
+    }.elsewhen (state === UInt(steadyState)) {  // STEADY
+      io.stageEnable.zip(doneMask).foreach { case (en, done) => en := ~done }
+
+      val doneTree = Bool(doneMask.reduce {_&_})
+      doneClear := doneTree
+      when(doneTree) {
+        when(ctr.io.data.out === (max - UInt(1))) {
+          stateFF.io.data.in := UInt(drainState)
+        }.otherwise {
+          stateFF.io.data.in := UInt(state)
+        }
+      }.otherwise {
+        stateFF.io.data.in := state
+      }
+    }.elsewhen (state < UInt(doneState)) {   // DRAIN
+      for ( i <- drainState until doneState) {
+        val drainStateID = i - drainState
+        when (state === UInt(i)) {
+          io.stageEnable.zip(doneMask).takeRight(numInputs - drainStateID - 1).foreach { case (en, done) => en := ~done }
+          io.stageEnable.dropRight(numInputs - drainStateID - 1).foreach { en => en := UInt(0) }
+
+          val doneTree = Bool(doneMask.takeRight(numInputs - drainStateID - 1).reduce {_&_})
+          doneClear := doneTree
+          when (doneTree) {
+            stateFF.io.data.in := UInt(i+1)
+          }.otherwise {
+            stateFF.io.data.in := state
+          }
+        }
+      }
+    }.elsewhen (state === UInt(doneState)) {  // DONE
+      stateFF.io.data.in := UInt(initState)
+    }.otherwise {
+      stateFF.io.data.in := state
+    }
+  }.otherwise {
+    stateFF.io.data.in := UInt(initState)
+  }
+
+  // Output logic
+  io.done := state === UInt(doneState)
+}
+
+
+class ParallelTests(c: Parallel) extends PlasticineTester(c) {
+  val numIter = 5
+  val stageIterCount = List.tabulate(c.numInputs) { i => math.abs(rnd.nextInt) % 10 }
+  println(s"stageIterCount: $stageIterCount")
+
+  def executeStages(s: List[Int]) {
+    val numCycles = s.map { stageIterCount(_) }
+    var elapsed = 0
+    var done: Int = 0
+    while (done != s.size) {
+      c.io.stageDone.foreach { poke(_, 0) }
+      step(1)
+      elapsed += 1
+      for (i <- 0 until s.size) {
+        if (numCycles(i) == elapsed) {
+          println(s"[Stage ${s(i)} Finished execution at $elapsed")
+          poke(c.io.stageDone(s(i)), 1)
+          done += 1
+        }
+      }
+    }
+    c.io.stageDone.foreach { poke(_, 1) }
+  }
+
+  def handleStageEnables = {
+    val stageEnables = c.io.stageEnable.map { peek(_).toInt }
+    val activeStage = stageEnables.zipWithIndex.filter { _._1 == 1 }.map { _._2 }
+//    executeStage(activeStage)
+  }
+
+  // Start
+  poke(c.io.numIter, numIter)
+  poke(c.io.enable, 1)
+
+  var done = peek(c.io.done).toInt
+  var numCycles = 0
+  while ((done != 1) & (numCycles < 100)) {
+    handleStageEnables
+    done = peek(c.io.done).toInt
+    step(1)
+    numCycles += 1
+  }
+}
+
+
+object ParallelTest {
+
+  def main(args: Array[String]): Unit = {
+    val (appArgs, chiselArgs) = args.splitAt(args.indexOf("end"))
+
+    val numInputs = 2
+    chiselMainTest(chiselArgs, () => Module(new Parallel(numInputs))) {
+      c => new ParallelTests(c)
+    }
+  }
+}
