7d7c8292d4a504f817087ae80e7638702de0a586
raghu <raghup17@gmail.com>
2016-11-17 15:52:51 -0800

    Adding the actual Parallel template

--- src/main/templates/Parallel.scala
+++ src/main/templates/Parallel.scala
@@ -11,37 +11,19 @@ class Parallel(val numInputs: Int) extends Module {
   val io = new Bundle {
     val enable = Bool(INPUT)
     val done = Bool(OUTPUT)
-    val numIter = UInt(INPUT, width=32)
     val stageEnable = Vec.fill(numInputs) { Bool(OUTPUT) }
     val stageDone = Vec.fill(numInputs) { Bool(INPUT) }
   }
 
   // 0: INIT, 1: RESET, 2..2+numInputs-1: stages, numInputs: DONE
   val initState = 0
-  val resetState = 1
-  val fillState = resetState + 1
-  val steadyState = fillState + numInputs - 1
-  val drainState = steadyState + 1
-  val doneState = drainState+numInputs-1
+  val steadyState = initState + 1
+  val doneState = steadyState + 1
 
   val stateFF = Module(new FF(32))
   stateFF.io.control.enable := Bool(true)
   stateFF.io.data.init := UInt(0)
   val state = stateFF.io.data.out
-
-  // Counter for num iterations
-  val maxFF = Module(new FF(32))
-  maxFF.io.control.enable := io.enable
-  maxFF.io.data.in := io.numIter
-  val max = maxFF.io.data.out
-
-  val ctr = Module(new Counter(32))
-  ctr.io.control.enable := io.enable & io.stageDone(0)
-  ctr.io.control.reset := (state === UInt(doneState))
-  ctr.io.control.saturate := Bool(false)
-  ctr.io.data.max := max
-  ctr.io.data.stride := UInt(1)
-  val iter = ctr.io.data.out
 
   val doneClear = UInt(width=1)
   val doneFF = List.tabulate(numInputs) { i =>
@@ -60,54 +42,16 @@ class Parallel(val numInputs: Int) extends Module {
 
   when(io.enable) {
     when(state === UInt(initState)) {   // INIT -> RESET
-      stateFF.io.data.in := UInt(resetState)
-    }.elsewhen (state === UInt(resetState)) {  // RESET -> FILL
-      stateFF.io.data.in := UInt(fillState)
-    }.elsewhen (state < UInt(steadyState)) {  // FILL -> STEADY
-      for ( i <- fillState until steadyState) {
-        val fillStateID = i - fillState
-        when((state === UInt(i))) {
-          io.stageEnable.zip(doneMask).take(fillStateID+1).foreach { case (en, done) => en := ~done }
-          io.stageEnable.drop(fillStateID+1).foreach { en => en := UInt(0) }
-          val doneTree = Bool(doneMask.take(fillStateID+1).reduce {_&_})
-          doneClear := doneTree
-
-          when (doneTree) {
-            stateFF.io.data.in := UInt(i+1)
-          }.otherwise {
-            stateFF.io.data.in := state
-          }
-        }
-      }
+      stateFF.io.data.in := UInt(steadyState)
     }.elsewhen (state === UInt(steadyState)) {  // STEADY
       io.stageEnable.zip(doneMask).foreach { case (en, done) => en := ~done }
 
       val doneTree = Bool(doneMask.reduce {_&_})
       doneClear := doneTree
       when(doneTree) {
-        when(ctr.io.data.out === (max - UInt(1))) {
-          stateFF.io.data.in := UInt(drainState)
-        }.otherwise {
-          stateFF.io.data.in := UInt(state)
-        }
+        stateFF.io.data.in := UInt(doneState)
       }.otherwise {
-        stateFF.io.data.in := state
-      }
-    }.elsewhen (state < UInt(doneState)) {   // DRAIN
-      for ( i <- drainState until doneState) {
-        val drainStateID = i - drainState
-        when (state === UInt(i)) {
-          io.stageEnable.zip(doneMask).takeRight(numInputs - drainStateID - 1).foreach { case (en, done) => en := ~done }
-          io.stageEnable.dropRight(numInputs - drainStateID - 1).foreach { en => en := UInt(0) }
-
-          val doneTree = Bool(doneMask.takeRight(numInputs - drainStateID - 1).reduce {_&_})
-          doneClear := doneTree
-          when (doneTree) {
-            stateFF.io.data.in := UInt(i+1)
-          }.otherwise {
-            stateFF.io.data.in := state
-          }
-        }
+        stateFF.io.data.in := UInt(state)
       }
     }.elsewhen (state === UInt(doneState)) {  // DONE
       stateFF.io.data.in := UInt(initState)
@@ -154,7 +98,6 @@ class ParallelTests(c: Parallel) extends PlasticineTester(c) {
   }
 
   // Start
-  poke(c.io.numIter, numIter)
   poke(c.io.enable, 1)
 
   var done = peek(c.io.done).toInt
