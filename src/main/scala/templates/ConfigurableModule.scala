bdda98dbbeb757f881cd2f730673c781337e74a7
raghu <raghup17@gmail.com>
2017-04-09 01:03:50 -0700

    Comment out CoalescingCache and ConfigurableModule for now

--- src/main/scala/templates/ConfigurableModule.scala
+++ src/main/scala/templates/ConfigurableModule.scala
@@ -1,27 +1,27 @@
-package plasticine.templates
-
-import Chisel._
-
-import scala.collection.mutable.HashMap
-
-/**
- * abstract configurable interface
- */
-abstract class OpcodeT extends Bundle
-
-abstract class ConfigInterface extends Bundle {
-  val config_data = Bool(INPUT)
-}
-
-/**
- * Abstract 'ConfigurableModule' class. This class is a thin wrapper
- * around a standard Chisel Module with a few extra parameters
- * and methods related to reconfiguration logic to be defined.
- */
-abstract class ConfigurableModule[M<:OpcodeT] extends Module {
-  val io: ConfigInterface
-
-  /** Storage for configuration bits */
-  val config : M
-
-}
+//package plasticine.templates
+//
+//import Chisel._
+//
+//import scala.collection.mutable.HashMap
+//
+///**
+// * abstract configurable interface
+// */
+//abstract class OpcodeT extends Bundle
+//
+//abstract class ConfigInterface extends Bundle {
+//  val config_data = Bool(INPUT)
+//}
+//
+///**
+// * Abstract 'ConfigurableModule' class. This class is a thin wrapper
+// * around a standard Chisel Module with a few extra parameters
+// * and methods related to reconfiguration logic to be defined.
+// */
+//abstract class ConfigurableModule[M<:OpcodeT] extends Module {
+//  val io: ConfigInterface
+//
+//  /** Storage for configuration bits */
+//  val config : M
+//
+//}
