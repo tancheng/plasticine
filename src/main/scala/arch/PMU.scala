5c0e9137fe181a53fe2feb05e1b0c49cf1f1da5f
raghu <raghup17@gmail.com>
2017-04-11 16:39:48 -0700

    Add a covariant type parameter to handle config bits

--- src/main/scala/arch/PMU.scala
+++ src/main/scala/arch/PMU.scala
@@ -46,5 +46,5 @@ case class PMUConfig(p: PMUParams) extends Bundle {
 }
 
 class PMU(val p: PMUParams) extends CU {
-  val io = IO(CUIO(p, Input(PMUConfig(p))))
+  val io = IO(CUIO(p, PMUConfig(p)))
 }
