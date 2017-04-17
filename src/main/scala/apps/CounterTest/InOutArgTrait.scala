f35238b98773606ffa1454f80be72754f665ae31
raghu <raghup17@gmail.com>
2017-04-17 00:27:59 -0700

    Add simple CounterTest to debug PISA

---
+++ src/main/scala/apps/CounterTest/InOutArgTrait.scala
@@ -0,0 +1,20 @@
+package plasticine.apps
+import plasticine.arch._
+import chisel3._
+import plasticine.spade._
+import plasticine.pisa.PISADesign
+import plasticine.pisa.ir._
+import chisel3.util._
+import scala.collection.mutable.ListBuffer
+import GeneratedTopParams.plasticineParams._
+import GeneratedTopParams._
+import plasticine.templates._
+import plasticine.pisa.enums._
+
+object CounterTest extends PISADesign with CounterTestTrait
+trait CounterTestTrait {
+
+  def main(args: String*) = {
+    CounterRCBits(max=SrcValueTuple(ConstSrc, 1), stride=SrcValueTuple(ConstSrc, 1), min=SrcValueTuple(ConstSrc, 1), par=1)
+  }
+}
