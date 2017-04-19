74aea3c25ec2569835e6c970e453ac1310246429
raghu <raghup17@gmail.com>
2017-04-19 01:38:20 -0700

    Reduce stage count to something more probable

--- src/main/scala/spade/gen/GeneratedParams.scala
+++ src/main/scala/spade/gen/GeneratedParams.scala
@@ -43,7 +43,7 @@ case class GeneratedPCUParams(override val numScalarIn:Int, override val numScal
   regColors += List(VecInReg,VecOutReg)
   regColors += List(VecInReg,VecOutReg)
   regColors += List(VecInReg,VecOutReg)
-  override val d = 20
+  override val d = 5
   override val r = regColors.size
 }
 
@@ -68,7 +68,7 @@ case class GeneratedPMUParams(override val numScalarIn:Int, override val numScal
   regColors += List(VecInReg)
   regColors += List(VecInReg)
   regColors += List(VecInReg)
-  override val d = 6
+  override val d = 5
   override val wd = 3
   override val r = regColors.size
 }
