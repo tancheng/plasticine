42033f76e10cc8fa55695ee57c4cad9ef79d5224
Yaqi Zhang <hsbzyq@gmail.com>
2017-04-22 16:59:38 -0700

    regenerate InOutArg change chain to be mutable

--- src/main/scala/apps/InOutArg/InOutArgTrait1.scala
+++ src/main/scala/apps/InOutArg/InOutArgTrait1.scala
@@ -21,8 +21,7 @@ trait InOutArgTrait1 {
     csbs(1)(0).outSelect(23) = 4
     csbs(1)(0).outSelect(24) = 15
     // Configuring cus(0)(0).asPCUBits <- PipeCU20_x192
-    val ctrs_0_0 = Array.tabulate(8) { i => CounterRCBits.zeroes(32)}
-    cus(0)(0).asPCUBits.counterChain = CounterChainBits(List(0,0,0,0,0,0,0), ctrs_0_0)
+    cus(0)(0).asPCUBits.counterChain.chain = List(0,0,0,0,0,0,0)
     cus(0)(0).asPCUBits.control.tokenInAndTree = List(0, 0, 0, 0, 0, 0, 0, 0)
     cus(0)(0).asPCUBits.control.fifoAndTree = List(0, 0, 0, 0, 0, 0, 0, 0)
     cus(0)(0).asPCUBits.control.siblingAndTree = List(1, 0, 0, 0, 0)
@@ -36,8 +35,11 @@ trait InOutArgTrait1 {
     // cus(0)(0).asPCUBits.scalarOutXbar=[None,None,Some(pr(st5512[8],reg13[8])),None]
     cus(0)(0).asPCUBits.scalarOutXbar.outSelect(2) = 0
     cus(0)(0).asPCUBits.counterChain.counters(0) = CounterRCBits(max=SVT(ConstSrc, 1), stride=SVT(ConstSrc, 1), min=SVT(ConstSrc, 1), par=1)
-    cus(0)(0).asPCUBits.stages = Array.tabulate(9) { i => PipeStageBits.zeroes(16, 32)}
-    cus(0)(0).asPCUBits.stages(0) = PipeStageBits(SVT(ScalarFIFOSrc, 0),SVT(ConstSrc, 4),SVT(), FixAdd, List(SVT(CurrStageDst, 8),SVT(CurrStageDst, 0)), Array.tabulate(16) { i => SVT() }, SVT(ConstSrc, 1))
+    cus(0)(0).asPCUBits.stages(0).opA = SVT(ScalarFIFOSrc, 0)
+    cus(0)(0).asPCUBits.stages(0).opB = SVT(ConstSrc, 4)
+    cus(0)(0).asPCUBits.stages(0).opC = SVT()
+    cus(0)(0).asPCUBits.stages(0).opcode = FixAdd
+    cus(0)(0).asPCUBits.stages(0).res = List(SVT(CurrStageDst, 8),SVT(CurrStageDst, 0))
     cus(0)(0).asPCUBits.stages(0).fwd(0) = SVT(ALUSrc, 0)
     cus(0)(0).asPCUBits.stages(0).fwd(8) = SVT(ALUSrc, 0)
     cus(0)(0).asPCUBits.stages(1).fwd(8) = SVT(PrevStageSrc, 8)
@@ -49,13 +51,9 @@ trait InOutArgTrait1 {
     cus(0)(0).asPCUBits.stages(7).fwd(8) = SVT(PrevStageSrc, 8)
     cus(0)(0).asPCUBits.stages(8).fwd(8) = SVT(PrevStageSrc, 8)
     // Configuring lcus(1)(0) <- SeqCU4_x193
-    val ctrs_1_0 = Array.tabulate(8) { i => CounterRCBits.zeroes(32)}
-    lcus(1)(0).counterChain = CounterChainBits(List(0,0,0,0,0, 0, 0), ctrs_1_0)
+    lcus(1)(0).counterChain.chain = List(0,0,0,0,0)
     lcus(1)(0).control.childrenAndTree = List(0, 0, 0, 0)
     lcus(1)(0).control.siblingAndTree = List(0, 0, 0, 0)
-    // lcus(1)(0).scalarInXbar=[None,None,None,None]
-    // lcus(1)(0).scalarOutXbar=[None,None]
     lcus(1)(0).counterChain.counters(0) = CounterRCBits(max=SVT(ConstSrc, 1), stride=SVT(ConstSrc, 1), min=SVT(ConstSrc, 1), par=1)
-//    lcus(1)(0).stages = Array.tabulate(0) { i => PipeStageBits.zeroes(0, 32)}
   }
 }
