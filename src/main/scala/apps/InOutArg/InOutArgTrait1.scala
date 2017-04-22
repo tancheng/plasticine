5161218955c49db205289d8ddc714eee2f0a5e33
Yaqi Zhang <hsbzyq@gmail.com>
2017-04-21 18:56:21 -0700

    generate InOutArg with control

--- src/main/scala/apps/InOutArg/InOutArgTrait1.scala
+++ src/main/scala/apps/InOutArg/InOutArgTrait1.scala
@@ -20,27 +20,33 @@ trait InOutArgTrait1 {
     csbs(1)(0).outSelect(22) = 17
     csbs(1)(0).outSelect(23) = 4
     csbs(1)(0).outSelect(24) = 15
-    // Configuring cus(0)(0) <- PipeCU20_x192
+    // Configuring cus(0)(0).asPCUBits <- PipeCU20_x192
     val ctrs_0_0 = Array.tabulate(8) { i => CounterRCBits.zeroes(32)}
-    val cc_0_0 = CounterChainBits(List(0,0,0,0,0,0,0), ctrs_0_0)
-    val sts_0_0 = Array.tabulate(9) { i => PipeStageBits.zeroes(16, 32)}
-    val scalarValidOuts = Array.tabulate(4) { i => SVT.zeroes(32) }
-    val vectorValidOuts = Array.tabulate(4) { i => SVT.zeroes(32) }
-    val control = PCUControlBoxBits.zeroes(cuParams(0)(0).asInstanceOf[PCUParams])
-    val scalarInXbar = CrossbarBits.zeroes(ScalarSwitchParams(4, 2, 32))
-    val scalarOutXbar = CrossbarBits.zeroes(ScalarSwitchParams(2, 4, 32))
-    cus(0)(0) = PCUBits(counterChain=cc_0_0, stages=sts_0_0, scalarValidOut=scalarValidOuts, vectorValidOut=vectorValidOuts, control = control, scalarInXbar = scalarInXbar, scalarOutXbar = scalarOutXbar)
-    cus(0)(0).counterChain.counters(0) = CounterRCBits(max=SVT(ConstSrc, 1), stride=SVT(ConstSrc, 1), min=SVT(ConstSrc, 1), par=1)
-    cus(0)(0).stages(0) = PipeStageBits(SVT(ScalarFIFOSrc, 0),SVT(ConstSrc, 4),SVT(), FixAdd, List(SVT(CurrStageDst, 0),SVT(CurrStageDst, 8)), Array.tabulate(16) { i => SVT() })
-    cus(0)(0).stages(0).fwd(0) = SVT(ALUSrc, 0)
-    cus(0)(0).stages(0).fwd(8) = SVT(ALUSrc, 0)
-    cus(0)(0).stages(1).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(2).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(3).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(4).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(5).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(6).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(7).fwd(8) = SVT(PrevStageSrc, 8)
-    cus(0)(0).stages(8).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.counterChain = CounterChainBits(List(0,0,0,0,0,0,0), ctrs_0_0)
+    cus(0)(0).asPCUBits.control.tokenInAndTree = List(0, 0, 0, 0, 0, 0, 0, 0)
+    cus(0)(0).asPCUBits.control.fifoAndTree = List(0, 0, 0, 0, 0, 0, 0, 0)
+    cus(0)(0).asPCUBits.control.siblingAndTree = List(1, 0, 0, 0, 0)
+    // PipeCU20_x192 isPipelining=true isStreaming=false
+    cus(0)(0).asPCUBits.control.streamingMuxSelect = 0
+    cus(0)(0).asPCUBits.control.incrementXbar.outSelect(0) = 5
+    cus(0)(0).asPCUBits.control.tokenOutXbar.outSelect(2) = 0
+    cus(0)(0).asPCUBits.control.doneXbar.outSelect(0) = 0
+    // cus(0)(0).asPCUBits.scalarInXbar=[Some(iw3234[2]),None,None,None]
+    cus(0)(0).asPCUBits.scalarInXbar.outSelect(0) = 2
+    // cus(0)(0).asPCUBits.scalarOutXbar=[None,None,Some(pr(st5512[8],reg13[8])),None]
+    cus(0)(0).asPCUBits.scalarOutXbar.outSelect(2) = 0
+    cus(0)(0).asPCUBits.counterChain.counters(0) = CounterRCBits(max=SVT(ConstSrc, 1), stride=SVT(ConstSrc, 1), min=SVT(ConstSrc, 1), par=1)
+    cus(0)(0).asPCUBits.stages = Array.tabulate(9) { i => PipeStageBits.zeroes(16, 32)}
+    cus(0)(0).asPCUBits.stages(0) = PipeStageBits(SVT(ScalarFIFOSrc, 0),SVT(ConstSrc, 4),SVT(), FixAdd, List(SVT(CurrStageDst, 8),SVT(CurrStageDst, 0)), Array.tabulate(16) { i => SVT() })
+    cus(0)(0).asPCUBits.stages(0).fwd(0) = SVT(ALUSrc, 0)
+    cus(0)(0).asPCUBits.stages(0).fwd(8) = SVT(ALUSrc, 0)
+    cus(0)(0).asPCUBits.stages(1).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(2).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(3).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(4).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(5).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(6).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(7).fwd(8) = SVT(PrevStageSrc, 8)
+    cus(0)(0).asPCUBits.stages(8).fwd(8) = SVT(PrevStageSrc, 8)
   }
 }
