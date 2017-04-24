1f0cce31339d159ad95f381cc27c66d96246a391
raghu <raghup17@gmail.com>
2017-04-24 12:16:06 -0700

    Minor updates to scripts and driver, add color printing support

--- src/main/scala/apps/SimpleSequential/Driver.cpp
+++ src/main/scala/apps/SimpleSequential/Driver.cpp
@@ -1,11 +1,12 @@
 #include "FringeContext.h"
+#include "PrettyPrintMacros.h"
 
 int main(int argc, char *argv[]) {
   // Create an execution context.
   FringeContext *c1 = new FringeContext("./accel.bit.bin");
   c1->load();
 //  time_t tstart = time(0);
-
+  printf("%s", KGRN);
   c1->setArg(0, 2);
   c1->setArg(1, 48);
   c1->setArg(2, 5);
@@ -14,6 +15,8 @@ int main(int argc, char *argv[]) {
 
   std::cout << "out = " << out << std::endl;
 
+  printf("%s", KNRM);
+  c1->setArg(0, 2);
   delete c1;
   return 0;
 }
