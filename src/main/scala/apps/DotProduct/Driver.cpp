7d51c8094a493bdfd83503efad1c019122b33411
raghu <raghup17@gmail.com>
2017-04-25 19:50:01 -0700

    Flesh out DotProduct driver more

--- src/main/scala/apps/DotProduct/Driver.cpp
+++ src/main/scala/apps/DotProduct/Driver.cpp
@@ -2,21 +2,52 @@
 #include "PrettyPrintMacros.h"
 
 int main(int argc, char *argv[]) {
+  ASSERT(argc >= 2, "Usage: %s <arraySize>\n", argv[0]);
+  int N = atoi(argv[1]);
+
   // Create an execution context.
   FringeContext *c1 = new FringeContext("./accel.bit.bin");
   c1->load();
 //  time_t tstart = time(0);
   printf("%s", KGRN);
-  c1->setArg(0, 2);
-  c1->setArg(1, 48);
-  c1->setArg(2, 5);
-  c1->run();
-  uint32_t out = c1->getArg(0);
 
-  std::cout << "out = " << out << std::endl;
+  // Malloc -> memcpy -> memcpy -> read out
+  uint32_t *hostbuf = (uint32_t*) malloc (N*sizeof(int));
+  uint32_t *hostbuf2 = (uint32_t*) malloc (N*sizeof(int));
+
+  for (int i = 0; i<N; i++) {
+    hostbuf[i] = i;
+    hostbuf2[i] = 0;
+  }
+
+  uint64_t deviceBuf = c1->malloc(N*sizeof(int));
+  c1->memcpy(deviceBuf, hostbuf, N*sizeof(int));
+
+  c1->memcpy(hostbuf2, deviceBuf, N*sizeof(int));
+
+  bool fail = false;
+  for (int i = 0; i<N; i++) {
+    if (hostbuf2[i] != hostbuf[i]) {
+      fail = true;
+      EPRINTF("ERROR: Mismatch at %d: Expected %u, found %u\n", i, hostbuf[i], hostbuf2[i]);
+    }
+  }
+
+  if (fail) {
+    printf("Test FAILED\n");
+  } else {
+    printf("Test PASSED\n");
+  }
+
+//  c1->setArg(0, 2);
+//  c1->setArg(1, 48);
+//  c1->setArg(2, 5);
+//  c1->run();
+//  uint32_t out = c1->getArg(0);
+
+//  std::cout << "out = " << out << std::endl;
 
   printf("%s", KNRM);
-  c1->setArg(0, 2);
   delete c1;
   return 0;
 }
