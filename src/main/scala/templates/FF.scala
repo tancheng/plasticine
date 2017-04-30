80d6742af11f246bfaff3c3542bd0325b9bedab9
raghu <raghup17@gmail.com>
2017-04-29 18:03:36 -0700

    Add FF with default value 0

--- src/main/scala/templates/FF.scala
+++ src/main/scala/templates/FF.scala
@@ -26,6 +26,23 @@ class FF(val w: Int) extends Module {
   io.out := ff
 }
 
+class FFNoInit(val w: Int) extends Module {
+  val io = IO(new Bundle {
+    val in   = Input(UInt(w.W))
+    val out  = Output(UInt(w.W))
+    val enable = Input(Bool())
+  })
+
+  val d = Wire(UInt(w.W))
+  val ff = RegNext(d, 0.U)
+  when (io.enable) {
+    d := io.in
+  } .otherwise {
+    d := ff
+  }
+  io.out := ff
+}
+
 class TFF(val w: Int) extends Module {
   val io = new Bundle {
     val out  = Output(UInt(w.W))
