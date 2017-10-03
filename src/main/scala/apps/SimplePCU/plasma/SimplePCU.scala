c2626d31ab57c43e9e2ffecb105b35be6afec414
raghu <raghup17@gmail.com>
2017-10-03 15:25:17 -0700

    Config fixes for reduction; both operands must not have 'ReduceTreeSrc'

--- src/main/scala/apps/SimplePCU/plasma/SimplePCU.scala
+++ src/main/scala/apps/SimplePCU/plasma/SimplePCU.scala
@@ -45,50 +45,48 @@ trait SimplePCUTrait {
   pcuBits.stages(0).opC = SVT()
   pcuBits.stages(0).opcode = FixMul
   pcuBits.stages(0).res = List(SVT(CurrStageDst, 0))
-  pcuBits.stages(0).fwd(0) = SVT(ALUSrc, 0)
+
   pcuBits.stages(1).fwd(0) = SVT(PrevStageSrc, 0)
   pcuBits.stages(2).opA = SVT(ReduceTreeSrc, 0)
-  pcuBits.stages(2).opB = SVT(ReduceTreeSrc, 0)
+  pcuBits.stages(2).opB = SVT(PrevStageSrc, 0)
   pcuBits.stages(2).opC = SVT()
   pcuBits.stages(2).opcode = FixAdd
   pcuBits.stages(2).res = List(SVT(CurrStageDst, 0))
-  pcuBits.stages(2).fwd(0) = SVT(ALUSrc, 2)
+
   pcuBits.stages(3).opA = SVT(ReduceTreeSrc, 0)
-  pcuBits.stages(3).opB = SVT(ReduceTreeSrc, 0)
+  pcuBits.stages(3).opB = SVT(PrevStageSrc, 0)
   pcuBits.stages(3).opC = SVT()
   pcuBits.stages(3).opcode = FixAdd
   pcuBits.stages(3).res = List(SVT(CurrStageDst, 0))
-  pcuBits.stages(3).fwd(0) = SVT(ALUSrc, 3)
+
   pcuBits.stages(4).opA = SVT(ReduceTreeSrc, 0)
-  pcuBits.stages(4).opB = SVT(ReduceTreeSrc, 0)
+  pcuBits.stages(4).opB = SVT(PrevStageSrc, 0)
   pcuBits.stages(4).opC = SVT()
   pcuBits.stages(4).opcode = FixAdd
   pcuBits.stages(4).res = List(SVT(CurrStageDst, 0))
-  pcuBits.stages(4).fwd(0) = SVT(ALUSrc, 4)
+
   pcuBits.stages(5).opA = SVT(ReduceTreeSrc, 0)
-  pcuBits.stages(5).opB = SVT(ReduceTreeSrc, 0)
+  pcuBits.stages(5).opB = SVT(PrevStageSrc, 0)
   pcuBits.stages(5).opC = SVT()
   pcuBits.stages(5).opcode = FixAdd
   pcuBits.stages(5).res = List(SVT(CurrStageDst, 0))
-  pcuBits.stages(5).fwd(0) = SVT(ALUSrc, 5)
+
   pcuBits.stages(6).opA = SVT(PrevStageSrc, 0)
   pcuBits.stages(6).opB = SVT(CurrStageSrc, 1)
   pcuBits.stages(6).opC = SVT()
   pcuBits.stages(6).opcode = FixAdd
   pcuBits.stages(6).res = List(SVT(CurrStageDst, 1))
   pcuBits.accumInit = 0
-  pcuBits.stages(6).fwd(1) = SVT(ALUSrc, 6)
+
   pcuBits.stages(7).opA = SVT(PrevStageSrc, 1)
   pcuBits.stages(7).opB = SVT()
   pcuBits.stages(7).opC = SVT()
   pcuBits.stages(7).opcode = BypassA
-  pcuBits.stages(7).res = List(SVT(CurrStageDst, 5))
-  pcuBits.stages(7).fwd(5) = SVT(ALUSrc, 7)
+  pcuBits.stages(7).res = List(SVT(CurrStageDst, 2))
 
   // Route scalar to output
-  pcuBits.scalarOutXbar.outSelect(1) = 0
+  pcuBits.scalarOutXbar.outSelect(1) = 2
 
-  // Control: Just use tokenIn
 //  pcuBits.control.udcInit=List(2,-1,-1,-1,-1)
 //  pcuBits.control.tokenInAndTree = List(0, 0, 0, 0)
 //  pcuBits.control.fifoAndTree = List(0, 0, 0, 0, 1, 1, 0, 0)
