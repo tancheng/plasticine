17aa626d2210fe0d2747847f4d5b5b286ec73a0e
raghu <raghup17@gmail.com>
2017-10-04 17:40:33 -0700

    1. Add target name to generated folder 2. Use correct path to vcs simulation headers in build scripts

--- static/vcs/sw/run.sh
+++ static/vcs/sw/run.sh
@@ -2,15 +2,15 @@
 
 #TOP=PMU
 source simulator.mk
-export SIM_DIR=${PLASTICINE_HOME}/sim_out/$TOP
+export SIM_DIR=${PLASTICINE_HOME}/sim_out/${TOP}_vcs
 export SIM_PATH=${SIM_DIR}/psim.bin
 export DRAMSIM_HOME=${SIM_DIR}/hw/DRAMSim2
 export LD_LIBRARY_PATH=$DRAMSIM_HOME:$LD_LIBRARY_PATH
 export USE_IDEAL_DRAM=0
 export DRAM_DEBUG=0
 export DRAM_NUM_OUTSTANDING_BURSTS=-1
-export VPD_ON=1
-export VCD_ON=1
+export VPD_ON=0
+export VCD_ON=0
 
 ./Top $@ 2>&1 | tee sim.log
 
